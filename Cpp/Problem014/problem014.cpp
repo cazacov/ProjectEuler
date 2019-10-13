2e40378d5d2d39f101771356f809d7fcb021d330
Victor Cazacov <cazacov.git@gmail.com>
2019-10-13 23:36:41 +0200

    Refactoring: improve performance by replacing the map with a fixed size array

--- Cpp/Problem014/problem014.cpp
+++ Cpp/Problem014/problem014.cpp
@@ -18,24 +18,30 @@ Which starting number, under one million, produces the longest chain?
  */
 
 #include <iostream>
-#include <map>
 #include <vector>
 
+const int UPPER_LIMIT = 1000000;
+const int HISTORY_SIZE = UPPER_LIMIT * 4;
+int known_numbers[HISTORY_SIZE] = {0};
+
 int main() {
-
-    const int UPPER_LIMIT = 1000000;
-
-    std::map<int, int> known_numbers {{1,1}};
-    std::vector<int> path;
+    known_numbers[1] = 1;
+    std::vector<long> path;
 
     int max_sequence_length = 1;
     int max_starting_number = 1;
 
     for (int n = 2; n < UPPER_LIMIT; n++) {
+        if (known_numbers[n] != 0) {
+            // We have already hit that element while generating sequence for some other number
+            continue;
+        };
+
         path.clear();
-        unsigned int element = n;
-        auto existing_it = known_numbers.find(element);
-        while (existing_it == known_numbers.end()) {
+        unsigned long element = n;
+
+        // Repeat while the length of the Collatz sequence is not known yet (not in the cache)
+        while (element >= HISTORY_SIZE || !known_numbers[element]) {
             path.push_back(element);
             if (element & 0x01) {
                 element = element *3 + 1;
@@ -43,18 +49,20 @@ int main() {
             else {
                 element >>= 1;
             }
-            existing_it = known_numbers.find(element);
         }
-        if (path.empty()) {
-            continue;
-        }
-        auto sequence_length = (*existing_it).second;
+        auto sequence_length = known_numbers[element];
         if (sequence_length + path.size() > max_sequence_length) {
             max_sequence_length = sequence_length + path.size();
             max_starting_number = path[0];
         }
+
+        // Put in the cache the sequence length for all elements in the path
         for (auto rev_it = path.rbegin(); rev_it != path.rend(); ++rev_it ) {
-            known_numbers[*rev_it] = ++sequence_length;
+            if (*rev_it < HISTORY_SIZE) {
+                known_numbers[*rev_it] = ++sequence_length;
+            } else{
+                ++sequence_length;
+            }
         }
     }
     std::cout << max_starting_number << std::endl;
