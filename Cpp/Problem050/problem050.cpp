609a75af4aefee8291c8b3ebcfee887d5751235a
Victor Cazacov <cazacov.git@gmail.com>
2019-11-11 22:10:20 +0100

    Refatoring: use iterators instead of positions

--- Cpp/Problem050/problem050.cpp
+++ Cpp/Problem050/problem050.cpp
@@ -11,34 +11,37 @@
  */
 
 #include <iostream>
+#include <iterator>
+#include <numeric>
 #include "PrimesInRange.h"
 
 int main() {
 
     const int N = 1000000;
     PrimesInRange primesPool(N);
-    int max_length = 1;
-    int max_pos = 0;
 
     std::vector<int> primes;
     primes.assign(primesPool.primes.begin(), primesPool.primes.end());
 
-    for (int i = primes.size()-1; i >max_length; i--) {
-        int candidate = primes[i];
+    long max_length = 20; // We know there exits a sum with at least 21 terms
+    std::vector<int, std::allocator<int>>::iterator max_pos;
 
-        int head = 1;
-        int tail = 0;
-        int sum = primes[tail];
+    for (auto it = primes.rbegin(); it != primes.rend(); it++) {
+
+        auto candidate = *it;
+        auto tail = primes.begin();
+        auto head = primes.begin();
+        int sum = 0;
 
         do {
-            while (head <= i && sum < candidate) {
-                sum += primes[head];
-                head++;
+            // move head
+            while (*head < candidate && sum < candidate) {
+                sum += *head++;
             }
 
+            // move tail
             while (tail < head && sum > candidate) {
-                sum -= primes[tail];
-                tail++;
+                sum -= *tail++;
             }
 
             if (sum == candidate)
@@ -49,14 +52,15 @@ int main() {
                 }
                 break;
             }
-        } while(head < i &&  head-tail > max_length);
+        } while(head-tail > max_length);
     }
 
-    int s = 0;
-    for (int i = 0; i < max_length; i++) {
-        std::cout << primes[i+max_pos] << std::endl;
-        s+=primes[i+max_pos];
-    }
-    std::cout << "Answer: " << s << std::endl;
+    int result = std::accumulate(max_pos, max_pos + max_length, 0,
+            [](int acc, int elem) {
+                std::cout << elem << std::endl;
+                return acc+elem;
+            });
+    std::cout << "Answer: The prime number " << result << " is a sum of " << max_length << " consecutive primes." << std::endl;
+
     return 0;
 }
